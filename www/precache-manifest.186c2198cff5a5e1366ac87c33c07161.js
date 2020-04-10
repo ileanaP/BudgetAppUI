79f52c524376c9729c09e402b4d422dcbc9f443f
IleanaP <ileana.profeanu@gmail.com>
2020-04-10 15:13:55 +0300

    init commit

---
+++ www/precache-manifest.186c2198cff5a5e1366ac87c33c07161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400d17cc3e18885e3051f875e79e1b1b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "385f20f3b7bb0278133b",
+    "url": "./static/css/main.567b1c2b.chunk.css"
+  },
+  {
+    "revision": "01fd2db0fe8bac2a5bff",
+    "url": "./static/js/2.40bb519f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.40bb519f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "385f20f3b7bb0278133b",
+    "url": "./static/js/main.3e0d0e8d.chunk.js"
+  },
+  {
+    "revision": "d83042418d9e4a7c73a7",
+    "url": "./static/js/runtime-main.7f9e9628.js"
+  }
+]);
